409498e61cca3016e435b5d79516b328bb75d5c2
Ravinder <ravinder729090@gmail.com>
2026-03-26 18:01:35 +0530

    feat: add create post structure

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -4,7 +4,30 @@ import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 
 export const createPost = asyncHandler(async (req, res) => {
+    const userId = req.user?._id
+    const { text } = req.body;
+    const file = req.file
 
+    if (!text && !file) {
+        throw new ApiError(400, "Post must contain text or image")
+    }
+
+    const newPost = await Post.create({
+        author: userId,
+        text,
+        image: {
+            imageUrl,
+            publicUrl
+        }
+    })
+
+    const post = await Post
+        .findById(newPost._id)
+        .populate("author", "name email")
+
+    return res
+        .status(201)
+        .json(new ApiResponse(201, post, "Post created"))
 });
 
 export const getFeed = asyncHandler(async (req, res) => {
