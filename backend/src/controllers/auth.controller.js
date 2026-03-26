efbf0504662ffb5e073bf74f21560a27c771af22
Ravinder <ravinder729090@gmail.com>
2026-03-26 17:08:15 +0530

    feat: implement user login API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -36,5 +36,21 @@ export const signup = asyncHandler(async (req, res) => {
 });
 
 export const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email | !password) throw new ApiError(400, "All fields are required")
+
+    const user = await User.findOne({ email });
+
+    if (!user || !user.matchPassword(password)) throw new ApiError(404, "Email or Password is incorrect")
+
+    const responseData = {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id)
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, responseData, "User loggined successfully"));
 });
