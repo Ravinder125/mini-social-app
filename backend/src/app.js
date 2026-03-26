52d87ccc72bb149061dccbe5032f5c037fbaba81
Ravinder <ravinder729090@gmail.com>
2026-03-26 16:00:57 +0530

    chore: register global error middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from 'morgan'
+import errorMiddleware from './middleware/error.middleware'
 
 dotenv.config();
 
@@ -10,7 +11,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(errorMiddleware);
+app.use(morgan("dev"));
 
 app.get("/", (_, res) => {
     res.send("API running");
