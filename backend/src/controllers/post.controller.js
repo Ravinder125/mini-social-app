f08e5ccd94595d4b2124261c4ecca4eb305d804f
Ravinder <ravinder729090@gmail.com>
2026-03-26 17:51:19 +0530

    feat: add post controller structure

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,20 @@
+import Post from "../models/post.model.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+
+export const createPost = asyncHandler(async (req, res) => {
+
+});
+
+export const getFeed = asyncHandler(async (req, res) => {
+
+});
+
+export const likePost = asyncHandler(async (req, res) => {
+
+});
+
+export const addComment = asyncHandler(async (req, res) => {
+
+});
