42672c99c36a35048e48f1b60aadd414d26556f6
Ravinder <ravinder729090@gmail.com>
2026-03-26 17:50:05 +0530

    feat: create post model with likes and comments support

--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -1,18 +1,17 @@
 const errorHandler = (err, req, res, next) => {
 
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let statusCode = res.statusCode ?? 500;
 
-    res.status(statusCode);
+    console.log(err)
+    return res
+        .status(statusCode)
+        .json({
+            message: err.message,
+            stack: process.env.NODE_ENV === "production"
+                ? null
+                : err.stack
 
-    res.json({
-
-        message: err.message,
-
-        stack: process.env.NODE_ENV === "production"
-            ? null
-            : err.stack
-
-    });
+        });
 
 };
 
---
+++ backend/src/models/post.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    text: {
+        type: String,
+        required: true
+    }
+
+}, {
+    timestamps: true
+});
+
+const postSchema = new mongoose.Schema({
+
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    text: {
+        type: String
+    },
+
+    image: {
+        imageUrl: {
+            type: String,
+            trim: true
+        },
+        publicUrl: {
+            type: String,
+            trim: true
+        }
+    },
+
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+
+    comments: [commentSchema]
+
+}, {
+    timestamps: true
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
