c25ab9faed0b1413f3f6431262350186d99a1e08
Ravinder <ravinder729090@gmail.com>
2026-03-26 15:54:08 +0530

    chore: install and setup morgan for loggin in development

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from 'morgan'
 
 dotenv.config();
 
@@ -9,8 +10,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.use(morgan("dev"))
 
-app.get("/", (req,res)=>{
+app.get("/", (_, res) => {
     res.send("API running");
 });
 
