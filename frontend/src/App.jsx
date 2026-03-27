661daf2fc06b958cf2154b8cabcdcf28578ced58
Ravinder <ravinder729090@gmail.com>
2026-03-27 16:22:16 +0530

    feat: setup frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { lazy } from "react"
+import { Route, Routes } from "react-router-dom"
+
+const Login = lazy(() => import("./pages/Login"))
+const Social = lazy(() => import("./pages/Social"))
+const Signup = lazy(() => import("./pages/Signup"))
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Social />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   )
 }
 
