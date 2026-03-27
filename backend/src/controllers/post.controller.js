e364ffbd7ce23bb8dbcfef16963e3c98be8460d1
Ravinder <ravinder729090@gmail.com>
2026-03-27 14:54:44 +0530

    refactor: refactor create post controller to upload image

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -2,22 +2,32 @@ import Post from "../models/post.model.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 export const createPost = asyncHandler(async (req, res) => {
     const userId = req.user?._id
     const { text } = req.body;
-    const file = req.file
-
+    const file = req.file;
+    
     if (!text && !file) {
         throw new ApiError(400, "Post must contain text or image")
+    }
+
+    let postImageOjb = {}
+
+    if (file) {
+        const filePath = file?.path;
+        const uploadImage = await uploadOnCloudinary(filePath)
+        const { url = null, public_id = null } = uploadImage;
+        postImageOjb = { url, public_id }
     }
 
     const newPost = await Post.create({
         author: userId,
         text,
         image: {
-            imageUrl,
-            publicUrl
+            url: postImageOjb.url,
+            public_id: postImageOjb.public_id
         }
     })
 
