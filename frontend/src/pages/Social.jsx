ef870cb6f115a164362c4d8307e1e2ca2bab5438
Ravinder <ravinder729090@gmail.com>
2026-03-28 13:27:23 +0530

    feat: implement social page with post fetching

---
+++ frontend/src/pages/Social.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import API from "../api/axios";
+import PostCard from "../components/PostCard";
+import CreatePost from "../components/CreatePost";
+
+function Social() {
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchPosts = async () => {
+        try {
+            const res = await API.get("/posts");
+            setPosts(res.data.data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    if (loading) return <h3>Loading...</h3>
+
+    return (
+
+        <div>
+            <h2>Social Feed</h2>
+            <CreatePost refresh={fetchPosts} />
+            {
+                posts.map(post => (
+
+                    <PostCard
+                        key={post._id}
+                        post={post}
+                    />
+                ))
+            }
+
+        </div>
+
+    );
+
+}
+
+export default Social;
