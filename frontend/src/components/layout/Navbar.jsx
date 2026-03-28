e883dd7151d61a90b616d58d0a2623f276b3dc3f
Ravinder <ravinder729090@gmail.com>
2026-03-28 13:51:02 +0530

    feat: add navbar with logout funtionality

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import { AuthContext } from "../context/AuthContext";
+
+function Navbar() {
+    const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/");
+    };
+
+    return (
+
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "10px",
+                borderBottom: "1px solid gray"
+            }}
+        >
+
+            <h3>
+                Mini Social
+            </h3>
+
+            <div>
+                <span>
+                    {user?.name}
+                </span>
+                <button
+                    onClick={handleLogout}
+                    style={{
+                        marginLeft: "10px"
+                    }}
+                >
+                    Logout
+                </button>
+
+            </div>
+
+        </div>
+
+    );
+
+}
+
+export default Navbar;
