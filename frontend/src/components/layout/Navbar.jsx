3cfa70a8360c4b18746d1373fe26a34ea11b6cd2
Ravinder <ravinder729090@gmail.com>
2026-03-28 16:05:20 +0530

    refactor: import use auth hook to replace use context hook use

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,11 +1,8 @@
-import { useContext } from "react";
-
 import { useNavigate } from "react-router-dom";
-
-import { AuthContext } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 function Navbar() {
-    const { user, logout } = useContext(AuthContext);
+    const { user, logout } = useAuth();
     const navigate = useNavigate();
 
     const handleLogout = () => {
