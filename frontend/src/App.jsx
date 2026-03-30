3c4c8a3c65eae66f35b924d9675e46ca7b86ffe3
Ravinder <ravinder729090@gmail.com>
2026-03-30 14:01:22 +0530

    refactor: wrap social route and post with ID route protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import { lazy } from "react"
 import { Route, Routes } from "react-router-dom"
+import ProtectRoute from "./components/ProtectRoute"
 
 const Login = lazy(() => import("./pages/Login"))
 const Social = lazy(() => import("./pages/Social"))
 const Signup = lazy(() => import("./pages/Signup"))
+const PostDetails = lazy(() => import("./pages/PostDetails"))
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Social />} />
+      <Route element={<ProtectRoute />}>
+        <Route path="/" element={<Social />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
