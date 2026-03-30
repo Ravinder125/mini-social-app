fbc27c7f9bf8ce7b68ba0272a2bc2822997728e9
Ravinder <ravinder729090@gmail.com>
2026-03-30 13:44:01 +0530

    feat: implement protected to protect login required routes

---
+++ frontend/src/components/ProtectRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
+
+const ProtectRoute = () => {
+  const { user } = useAuth()
+
+  if (!user) return <Navigate to="/login" />
+  return <Outlet />
+}
+
+export default ProtectRoute
